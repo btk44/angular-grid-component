9688f39d9987a363bde3f003a46ddb8b31161428
bk <bartosz.komarnicki@gmail.com>
2022-02-03 18:26:14 +0100

    grid rebuilt to components -started

--- angular-app/src/app/components/configurable-editable-grid/grid/grid-row/grid-row.component.ts
+++ angular-app/src/app/components/configurable-editable-grid/grid/grid-row/grid-row.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GridColumnSetup } from '../../grid-column-setup';
 
 @Component({
-  selector: 'app-grid-row',
+  selector: '[app-grid-row]',
   templateUrl: './grid-row.component.html',
   styleUrls: ['./grid-row.component.scss']
 })
 export class GridRowComponent implements OnInit {
+
+  @Input() dataRow: any;
+  @Input() columnsSetup: GridColumnSetup[] = [];
+
+  editMode = false;
+  inputTypeMap = { 'string' : 'text', 'number': 'number', 'boolean' : 'checkbox'}
 
   constructor() { }
 
--- angular-app/src/app/components/configurable-editable-grid/grid/grid.component.ts
+++ angular-app/src/app/components/configurable-editable-grid/grid/grid.component.ts
@@ -9,12 +9,28 @@ import { GridColumnSetup } from '../grid-column-setup';
 export class GridComponent implements OnInit {
 
   gridColumnSetup: GridColumnSetup[] = [];
+  gridRows: any[] = [];
+  addingModeOn: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.gridColumnSetup = JSON.parse(localStorage.getItem('grid-setup')?? '{}');
+    this.gridColumnSetup = JSON.parse(localStorage.getItem('grid-setup')?? '{}') ?? [];
+    
+    // test
+    if(this.gridColumnSetup.length){
+      for(let i=0; i<6; i++){
+        let newRow: any = [];
+        for(let c=0; c<this.gridColumnSetup.length; c++){
+          newRow[this.gridColumnSetup[c].name] = 'test';
+        }
 
+        this.gridRows.push(newRow);
+      }
+    }
   }
 
+  onAddRow(): void {
+      this.addingModeOn = !this.addingModeOn;
+  }
 }
